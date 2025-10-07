280068c89c4cefb0dc605b4c1e57ea5775394644
battlystudios-coder <battlystudios50@gmail.com>
2025-10-07 09:43:00 +0200

    v1.0.0

---
+++ src/assets/js/windows/updateWindow.js
@@ -0,0 +1,52 @@
+"use strict";
+const electron = require("electron");
+const path = require("path");
+const os = require("os");
+let updateWindow = undefined;
+
+function getWindow() {
+    return updateWindow;
+}
+
+function destroyWindow() {
+    if (!updateWindow) return;
+    updateWindow.close();
+    updateWindow = undefined;
+}
+
+async function createWindow() {
+    destroyWindow();
+    updateWindow = new electron.BrowserWindow({
+        title: "Avvio di Battly",
+        width: 400,
+        height: 500,
+        resizable: false,
+        icon: `./src/assets/images/icon.${os.platform() === "win32" ? "ico" : "png"}`,
+        transparent: os.platform() === 'win32',
+        frame: false,
+        show: true,
+        webPreferences: {
+            contextIsolation: false,
+            nodeIntegration: true,
+            devTools: false
+        },
+    });
+
+    
+
+
+    electron.Menu.setApplicationMenu(null);
+    updateWindow.setMenuBarVisibility(false);
+        updateWindow.loadFile(path.join(electron.app.getAppPath(), 'src', 'index.html'));
+        updateWindow.once('ready-to-show', () => {
+            if (updateWindow) {
+                updateWindow.openDevTools();
+            }
+        });
+}
+
+module.exports = {
+    getWindow,
+    createWindow,
+    destroyWindow,
+};
